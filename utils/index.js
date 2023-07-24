93775c52507104f0b1e259b59994ae38c5fde635
Pedro Muniz <46171624+salesp07@users.noreply.github.com>
2023-07-24 13:14:55 -0700

    Add new utils files to index.js

--- utils/index.js
+++ utils/index.js
@@ -2,3 +2,5 @@
 
 export { default as getTokenAt } from './get-token-at';
 export { default as hideSubstring } from './hide-substring';
+export { default as extractTextFromChildren } from './extract-text-from-children';
+export { default as getNewDelayMap } from './get-new-delay-map';
